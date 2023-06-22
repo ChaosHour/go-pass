f2ea01ac0fdacc319550c135edc0142977c1f36b
ChaosHour <kurt_lv@cox.net>
2023-06-22 15:05:32 -0700

    Adding functions and features

--- show_users.sql
+++ show_users.sql
@@ -1,3 +1,6 @@
 SET print_identified_with_as_hex = 1;
-SHOW CREATE USER 'root'@'%';
-SHOW CREATE USER 'root'@'localhost';
+SHOW CREATE USER 'chaoshour'@'%'; SHOW GRANTS FOR 'chaoshour'@'%';
+SHOW CREATE USER 'johnny5'@'%'; SHOW GRANTS FOR 'johnny5'@'%';
+SHOW CREATE USER 'klarsen'@'%'; SHOW GRANTS FOR 'klarsen'@'%';
+SHOW CREATE USER 'root'@'%'; SHOW GRANTS FOR 'root'@'%';
+SHOW CREATE USER 'root'@'localhost'; SHOW GRANTS FOR 'root'@'localhost';
